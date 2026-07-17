6aff009350b79159e5a802bd4148d77bafc49e1c
Hugo Sacaquirin <hsacaqui@pichincha.com>
2026-07-17 12:34:10 -0500

    add test

--- src/main/java/com/example/java_api/controller/Person.java
+++ src/main/java/com/example/java_api/controller/Person.java
@@ -7,6 +7,6 @@ import org.springframework.web.bind.annotation.RestController;
 public class Person {
     @GetMapping("/saludo")
     public String getMessage() {
-        return "¡Hola desde el controlador de Spring Boot!";
+        return "¡Hola desde el controlador de Spring Boot! desplegado en AWS/ECR/ECS-task definition";
     }
 }
